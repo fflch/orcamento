fe417bcdf4adba7556262d97bcb56babb97bd49b
Flavia Lima <flavialima2002@usp.br>
2023-08-29 10:16:50 -0300

    att

--- resources/views/alternateindex.blade.php
+++ resources/views/alternateindex.blade.php
@@ -4,8 +4,8 @@
     @parent
     <div class="card p-3">
         <p>
-            <strong>Não foi possível localizar um ano ativo, portanto o login no sistema está indisponível.</strong><br>
-            Entre em contato com a Seção de Contabilidade.
+            <strong>Não foi possível localizar um ano ativo, portanto, o login no sistema está indisponível.</strong><br>
+            Entre em contato com o Serviço de Contabilidade.
         </p>
     </div>
 @stop
